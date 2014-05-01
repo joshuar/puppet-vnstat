be8cfeaaf9a619a3d3e546b5b4fd057e00379697
Joshua Rich <joshua.rich@gmail.com>
2014-05-01 22:13:50 +1000

    Fix syntax error.

--- manifests/vnstati/params.pp
+++ manifests/vnstati/params.pp
@@ -1,22 +1,22 @@
 class vnstat::vnstati::params inherits vnstat {
 
-  $header_format "%x %H:%M"
+  $header_format = '%x %H:%M'
   $show_hourly_rate = true
   $show_rate_in_summary = true
   $summary_layout = 'new'
   $transparent_bg = false
-  $image_cbackground = "FFFFFF"
-  $image_cedge = "AEAEAE"
-  $image_cheader = "606060"
-  $image_cheadertitle = "FFFFFF"
-  $image_cheaderdate = "FFFFFF"
-  $image_ctext = "000000"
-  $image_cline = "B0B0B0"
-  $image_clinel = "-"
-  $image_crx = "92CF00"
-  $image_ctx = "606060"
-  $image_crxd = "-"
-  $image_ctxd = "-"
+  $image_cbackground = 'FFFFFF'
+  $image_cedge = 'AEAEAE'
+  $image_cheader = '606060'
+  $image_cheadertitle = 'FFFFFF'
+  $image_cheaderdate = 'FFFFFF'
+  $image_ctext = '000000'
+  $image_cline = 'B0B0B0'
+  $image_clinel = '-'
+  $image_crx = '92CF00'
+  $image_ctx = '606060'
+  $image_crxd = '-'
+  $image_ctxd = '-'
 
   if $vnstat::use_vnstati {
     $package_ensure = 'installed'
@@ -36,5 +36,5 @@ class vnstat::vnstati::params inherits vnstat {
     }
   }
 
-  
+
 }
