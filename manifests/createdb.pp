4328530b6e4b2d72bc6da342babccb47b64ad087
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2015-02-15 17:16:09 +0100

    Remove path parameter from Exec.
    It's generally recommended to set a global path instead of having
    to modify each and every exec to set correct paths. For example
    on OpenBSD vnstat lives in /usr/local/bin.

--- manifests/createdb.pp
+++ manifests/createdb.pp
@@ -6,7 +6,6 @@ define vnstat::createdb ($label = undef) {
   }
 
   exec { "create-vnstat-db-${name}":
-    path => '/usr/bin:/bin',
     command => "vnstat ${args} ${name}",
     creates => "${vnstat::database_directory}/${name}",
     require => Package['vnstat'],
